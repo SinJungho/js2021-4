e49257d6a2a9e685e76a1498c6e277cea267c541
SinJungho <zmtls0516@naver.com>
2021-03-23 12:07:29 +0900

    4주차 2교시

--- hello.js
+++ hello.js
@@ -5,3 +5,28 @@ let name = "신"+"정"+"호";
 // 4주차
 
 console.log(`5+3 = ${5+3}`);
+
+// 대입 연산자
+let a = 10;
+
+a += 10;
+console.log(a);
+a -= 5;
+console.log(a);
+a *= 2;
+console.log(a);
+a /= 3;
+console.log(a);
+a %= 3;
+console.log(a);
+
+// 증감 연산자
+a++;
+console.log(a);
+
+// 자료형 검사
+
+typeof (123);
+typeof ("123");
+typeof (true);
+
