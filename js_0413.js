0ec8fbae845e6bc5ddb683cb04c60cb69ac80612
SinJungho <zmtls0516@naver.com>
2021-04-13 10:59:32 +0900

    7주차 1교시 필기 완료

---
+++ js_0413.js
@@ -0,0 +1,38 @@
+// let a = function(){
+//     console.log("첫 줄");
+//     console.log("두번째 줄");
+//     console.log("셋째 줄");
+// }
+
+// let bar = 123;
+
+// console.log(a);
+// console.log(bar);
+
+// 선언적 함수
+
+// function foo(){
+//     console.log("첫 줄");
+//     console.log("두번째 줄");
+// }
+
+// console.log(foo);
+
+// 화살표 함수
+
+// let foo = () =>{
+//     console.log("1st line");
+//     console.log("2st line");
+// }
+
+// foo();
+// console.log(foo); 
+
+// 함수 기본 형태
+
+function power(x){
+    return x * x;
+}
+
+console.log(power(20));
+console.log(power(10));
