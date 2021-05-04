25006b25c6c61b535a135ea1a236870b8ab2fa91
SinJungho <zmtls0516@naver.com>
2021-05-04 14:04:54 +0900

    코드

---
+++ js_0504.js
@@ -0,0 +1,22 @@
+/* let  primitiveNumber = 123;
+let foo = 1234;
+
+Number.prototype.method = function(){
+    return 'Primitive Method';
+}
+
+console.log(primitiveNumber.method());
+console.log(primitiveNumber.method()); */
+
+/* let numberA = Number.MAX_VALUE;
+let numberB = Number.MAX_VALUE+1000;
+
+console.log(numberA);
+console.log(numberB); */
+
+/* let foo = 'hello good morning';
+if(foo.indexOf('hello') >= 0){
+    console.log('hello good morning');
+}else{
+    console.log("안녕히 주무세요.");
+} */
